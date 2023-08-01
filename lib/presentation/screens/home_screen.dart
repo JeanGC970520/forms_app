7f9670a454117d7cda6b02078421f9dd00da5954
Jean Garcia <jeanpoolgarcia_97@hotmail.com>
2023-07-31 22:22:34 -0600

    Create home screen

---
+++ lib/presentation/screens/home_screen.dart
@@ -0,0 +1,25 @@
+
+
+import 'package:flutter/material.dart';
+
+class HomeScreen extends StatelessWidget {
+  const HomeScreen({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return  Scaffold(
+      body: ListView(
+        children: [
+
+          ListTile(
+            title: const Text('Cubits'),
+            subtitle: const Text('State management simple'),
+            trailing: const Icon( Icons.arrow_back_ios_rounded ),
+            onTap: () {} ,
+          ),
+
+        ],
+      ),
+    );
+  }
+}
