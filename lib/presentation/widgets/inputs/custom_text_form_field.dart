6396a6fa09beded7a9fe2f137f02400bf57b739e
Jean Garcia <jeanpoolgarcia_97@hotmail.com>
2023-08-03 22:35:21 -0600

    Finishing desing

--- lib/presentation/widgets/inputs/custom_text_form_field.dart
+++ lib/presentation/widgets/inputs/custom_text_form_field.dart
@@ -2,9 +2,23 @@ import 'package:flutter/material.dart';
 
 class CustomTextFormField extends StatelessWidget {
 
-  const CustomTextFormField({super.key});
+  const CustomTextFormField({
+    super.key,
+    this.label,
+    this.hintText,
+    this.errorMessage,
+    this.onChanged,
+    this.validator,
+    this.prefixIcon,
+  });
 
-
+  final String? label;
+  final String? hintText;
+  final String? errorMessage;
+  final Function(String)? onChanged;
+  final String? Function(String?)? validator;
+  final IconData? prefixIcon;
+ 
   @override
   Widget build(BuildContext context) {
 
@@ -16,15 +30,19 @@ class CustomTextFormField extends StatelessWidget {
     );
 
     return TextFormField(
-      onChanged: (value) {
-        print('value: $value');
-      },
-      validator: (value) {
-        return 'Error en el';
-      },
+      onChanged: onChanged,
+      validator: validator,
       decoration: InputDecoration(
         enabledBorder: border,
         focusedBorder: border.copyWith(borderSide: BorderSide(color: colors.primary)),
+        errorBorder: border.copyWith(borderSide: BorderSide(color: Colors.red.shade700)),
+        focusedErrorBorder: border.copyWith(borderSide: BorderSide(color: Colors.red.shade700)),
+        isDense: true, //Uses less space vertically
+        label: label != null ? Text(label!) : null,
+        hintText: hintText,
+        errorText: errorMessage,
+        focusColor: colors.primary,
+        prefixIcon: prefixIcon != null ? Icon( prefixIcon, ) : null,
       ),
     );
   }
