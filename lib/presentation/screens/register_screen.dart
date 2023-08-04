655870ea72cc2c3d3763d311de0dac4ec4566f84
Jean Garcia <jeanpoolgarcia_97@hotmail.com>
2023-08-03 22:00:26 -0600

    Creeate the base TextFormField

--- lib/presentation/screens/register_screen.dart
+++ lib/presentation/screens/register_screen.dart
@@ -20,6 +20,35 @@ class _RegisterView extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    return SafeArea(
+      child: Padding(
+        padding: const EdgeInsets.symmetric(horizontal: 10),
+        child: SingleChildScrollView(
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.end,
+            children: [
+        
+              const FlutterLogo( size: 100, ),
+        
+              TextFormField(),
+              TextFormField(),
+              TextFormField(),
+              TextFormField(),
+
+              const SizedBox(height: 30,),
+              
+              FilledButton.tonalIcon(
+                onPressed: () {},
+                icon: const Icon( Icons.save ), 
+                label: const Text('Save'),
+              ),
+
+              const SizedBox(height: 30,),
+              
+            ],
+          ),
+        ),
+      ),
+    );
   }
 }
