c49c4ee8024b47cd1cef3894c7d7b76f740660e9
Jean Garcia <jeanpoolgarcia_97@hotmail.com>
2023-08-04 21:40:29 -0600

    Defining the state structure

--- lib/presentation/blocs/register_cubit/register_state.dart
+++ lib/presentation/blocs/register_cubit/register_state.dart
@@ -1,11 +1,38 @@
 part of 'register_cubit.dart';
 
-class RegisterState extends Equatable {
-  const RegisterState();
 
-  // final String userName;
+enum FormStatus { invalid, valid, validating, posting }
+
+class RegisterFormState extends Equatable {
+
+  const RegisterFormState({
+    this.formStatus = FormStatus.invalid, 
+    this.userName = '', 
+    this.email = '', 
+    this.password = '', 
+  });
+
+  final FormStatus formStatus;
+  final String userName;
+  final String email;
+  final String password;
 
   @override
-  List<Object> get props => [];
+  List<Object> get props => [ formStatus, userName, email, password ];
+
+  RegisterFormState copyWith({
+    FormStatus? formStatus, 
+    String? userName,
+    String? email,
+    String? password,
+  }) {
+    return RegisterFormState(
+      formStatus: formStatus ?? this.formStatus,
+      userName: userName ?? this.userName,
+      email: email ?? this.email,
+      password: password ?? this.password,
+    );
+  }
+
 }
 
