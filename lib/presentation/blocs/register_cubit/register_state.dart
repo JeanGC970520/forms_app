b248089af544e36ad40c7d0b505515fad8dd2f9e
Jean Garcia <jeanpoolgarcia_97@hotmail.com>
2023-08-04 21:28:13 -0600

    Init Cubit

---
+++ lib/presentation/blocs/register_cubit/register_state.dart
@@ -0,0 +1,11 @@
+part of 'register_cubit.dart';
+
+class RegisterState extends Equatable {
+  const RegisterState();
+
+  // final String userName;
+
+  @override
+  List<Object> get props => [];
+}
+
