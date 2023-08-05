cf44aa75ce7bbfb409e141ac98c6ff583cef5622
Jean Garcia <jeanpoolgarcia_97@hotmail.com>
2023-08-05 11:58:23 -0600

    Using the Username formz to manage the username state and its TextFormField

--- lib/presentation/blocs/register_cubit/register_state.dart
+++ lib/presentation/blocs/register_cubit/register_state.dart
@@ -7,30 +7,34 @@ class RegisterFormState extends Equatable {
 
   const RegisterFormState({
     this.formStatus = FormStatus.invalid, 
-    this.userName = '', 
+    this.userName = const Username.pure(), 
     this.email = '', 
     this.password = '', 
+    this.isValid = false,
   });
 
   final FormStatus formStatus;
-  final String userName;
+  final Username userName;
   final String email;
   final String password;
+  final bool isValid;
 
   @override
-  List<Object> get props => [ formStatus, userName, email, password ];
+  List<Object> get props => [ formStatus, userName, email, password, isValid ];
 
   RegisterFormState copyWith({
     FormStatus? formStatus, 
-    String? userName,
+    Username? userName,
     String? email,
     String? password,
+    bool? isValid,
   }) {
     return RegisterFormState(
       formStatus: formStatus ?? this.formStatus,
       userName: userName ?? this.userName,
       email: email ?? this.email,
       password: password ?? this.password,
+      isValid:  isValid ?? this.isValid,
     );
   }
 
